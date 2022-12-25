363a2f43260d5e3e563f42a2385b0473d2dc6bac
K1000-O <cjeneconde@gmail.com>
2022-12-25 02:01:00 +0100

    Changed Human story.

--- src/player/race/Human.java
+++ src/player/race/Human.java
@@ -2,7 +2,16 @@ package player.race;
 
 /**
  * Human class.
- * First type of race abailable on the game. 
+ * First type of race abailable on the game.
+ * 
+ * Humans have rule over the kingdom since they had memories... But the ancient rulers
+ * wanted their power back. After the great war, Humans had to rebuild all the reign.
+ * 
+ * The great war changed everything... Old friendships became new enemies, old wealth
+ * is now buried six feet underground, old deals are now new war declarations.
+ * 
+ * Standard race, with standard stats. It's the easiest one but also the
+ * most homogeneous race.
  * 
  * @author Camilo Jené Conde <cjeneconde@gmail.com>
  * @version 0.0.1
