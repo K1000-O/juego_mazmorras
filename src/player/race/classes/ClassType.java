598aae81369caafc8a8955aeacf0c769f3de1b60
K1000-O <cjeneconde@gmail.com>
2022-12-28 22:56:11 +0100

    Modificated comment + inserted getters

--- src/player/race/classes/ClassType.java
+++ src/player/race/classes/ClassType.java
@@ -1,14 +1,20 @@
 package player.race.classes;
 
 /**
- * ClassType class.
- * Used to determinate the type of character class that we can choose:
- *      -  Ranged
- *      -  Warrior
- *      -  Mage
- *      -  Assassin
- * 
- * Each class type has different atributes.    
+ * {@code ClassType} class.
+ * <p>
+ * The {@code ClassType} indicates what path has chosen the player to follow. 
+ * Depending on what the {@link player.Player player} has chosen, the stats
+ * will variates.
+ * <p>  
+ * All the diferent classes that can be chosen are:
+ * <blockquote>
+ *      -  {@link Ranged Ranged}<p>
+ *      -  {@link Warrior Warrior}<p>
+ *      -  {@link Wizard Wizard}<p>
+ *      -  {@link Assassin Assassin}
+ * </blockquote>
+ * Each class type has different atributes and habilities.    
  * 
  * @author Camilo Jené Conde <cjeneconde@gmail.com>
  * @version 0.0.1
@@ -20,8 +26,32 @@ public abstract class ClassType {
     private int extraAttackSpeed;
     private int extraCriticalAttack;
 
-    public ClassType() {
+    public ClassType(int eA, int eD, int eAg, int eAS, int eCA) {
+        this.extraAttack = eA;
+        this.extraDefense = eD;
+        this.extraAgility = eAg;
+        this.extraAttackSpeed = eAS;
+        this.extraCriticalAttack = eCA;
+    }
 
+    public int getExtraAttack() {
+        return extraAttack;
+    }
+
+    public int getExtraDefense() {
+        return extraDefense;
+    }
+
+    public int getExtraAgility() {
+        return extraAgility;
+    }
+
+    public int getExtraAttackSpeed() {
+        return extraAttackSpeed;
+    }
+
+    public int getExtraCriticalAttack() {
+        return extraCriticalAttack;
     }
     
     public abstract int levelUp();
