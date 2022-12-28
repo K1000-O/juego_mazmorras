de0ee426855ba691d7c1133b2643687eaa28ae97
K1000-O <cjeneconde@gmail.com>
2022-12-28 22:55:46 +0100

    Created the different class types

---
+++ src/player/race/classes/Ranged.java
@@ -0,0 +1,16 @@
+package player.race.classes;
+
+public class Ranged extends ClassType{
+
+    public Ranged(int eA, int eD, int eAg, int eAS, int eCA) {
+        super(eA, eD, eAg, eAS, eCA);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public int levelUp() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
---
+++ src/player/race/classes/Wizard.java
@@ -0,0 +1,16 @@
+package player.race.classes;
+
+public class Wizard extends ClassType{
+
+    public Wizard(int eA, int eD, int eAg, int eAS, int eCA) {
+        super(eA, eD, eAg, eAS, eCA);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public int levelUp() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
